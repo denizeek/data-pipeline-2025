7ff6e311057b2fc37bbca952289ec3e214543177
denizeek <kuludeniz@gmail.com>
2025-12-19 01:38:20 +0300

    feat: initial project structure and borsa script

---
+++ borsa.py
@@ -0,0 +1,3 @@
+Bana içinde 'id', 'isim', 'fiyat' olan 10 satırlık sahte bir borsa verisi (list of dictionaries) oluştur.
+Sonra bu veriyi fiyatı 30'dan büyük olanları filtreleyip ekrana yazdıran bir Python kodu yaz.
+Tip belirteçlerini (type hints) kullan.
